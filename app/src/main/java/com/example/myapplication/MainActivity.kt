ad48acb25d27be0ae588df9c0730a404ea2c7838
Robebeck <razokkverihuk@gmail.com>
2024-01-21 21:02:23 -0500

    blarg

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("World")
-                    Greeting("Other World")
+                    Greeting("Other World?")
                 }
             }
         }
